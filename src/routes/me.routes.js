489bb995da4fc748d10e4ca2c8b9001b5496235f
thakurShaab-lab <thakurshady95@gmail.com>
2026-09-10 15:10:32 +0530

    Revert "Add GET /api/me for the chat header's Profile popup"
    This reverts commit ef666d3e79ec965f4333f24305cb49cfed2cbbb4.

--- src/routes/me.routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express'
-import * as meController from '../controllers/meController.js'
-import { asyncHandler } from '../middleware/asyncHandler.js'
-
-const router = Router()
-
-router.get('/', asyncHandler(meController.getMyProfile))
-
-export default router
