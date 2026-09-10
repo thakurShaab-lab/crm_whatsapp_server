9aaa9ecdff7c5415cd846de8734fab50393a5541
thakurShaab-lab <thakurshady95@gmail.com>
2026-09-10 16:26:55 +0530

    Reapply "Add GET /api/me for the chat header's Profile popup"
    This reverts commit 489bb995da4fc748d10e4ca2c8b9001b5496235f.

--- src/routes/index.js
+++ src/routes/index.js
@@ -8,6 +8,7 @@ import contactsRoutes from './contacts.routes.js'
 import templatesRoutes from './templates.routes.js'
 import webhooksRoutes from './webhooks.routes.js'
 import devRoutes from './dev.routes.js'
+import meRoutes from './me.routes.js'
 
 const router = Router()
 
@@ -22,6 +23,7 @@ router.use('/conversations', conversationsRoutes)
 router.use('/conversations/:mobile/messages', messagesRoutes)
 router.use('/contacts', contactsRoutes)
 router.use('/templates', templatesRoutes)
+router.use('/me', meRoutes)
 
 if (config.env !== 'production') {
   router.use('/dev', devRoutes)
